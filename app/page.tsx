efaa6cd9a6acb69ce6dada14fd91bf75bb081da3
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-07 09:32:14 +0500

    auto commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { connectDB } from "@/lib/dbConnection";
 import Image from "next/image";
 
 export default function Home() {
+  connectDB()
   return (
     <div>
       hi
     </div>
   );
 }
+
 // mongodb+srv://sohail:sohail786@cluster0.wqnr3vh.mongodb.net/
