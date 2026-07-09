b3b8d046426d091b8b4f654ee6cad4f930abde30
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-09 08:02:47 +0500

    auto commit

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest,NextResponse } from "next/server";
+import { connectDB } from "@/lib/dbConnection";
+import { hashedPassword } from "@/lib/auth";
+import User from "@/models/userModels";
+
+
+
