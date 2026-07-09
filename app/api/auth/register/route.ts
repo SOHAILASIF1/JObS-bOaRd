edfbe7068b09ac07fa2371dd9846b8d91ce3f56d
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-09 08:09:22 +0500

    auto commit

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -3,5 +3,15 @@ import { connectDB } from "@/lib/dbConnection";
 import { hashedPassword } from "@/lib/auth";
 import User from "@/models/userModels";
 
+export async function POST(req:NextRequest) {
+    try {
+        await connectDB()
+        
+    } catch (error) {
+        
+    }
+    
+}
+
 
 
