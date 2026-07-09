7e20ae71fd18aae8278da53def03b63c090ab3a9
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-09 08:27:01 +0500

    auto commit

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,11 +2,24 @@ import { NextRequest,NextResponse } from "next/server";
 import { connectDB } from "@/lib/dbConnection";
 import { hashedPassword } from "@/lib/auth";
 import User from "@/models/userModels";
+import { error } from "console";
 
 export async function POST(req:NextRequest) {
     try {
         await connectDB()
-        console.log("hi");
+        const {name,email,password,role}=await req.json()
+        if (!email ||!name || !password) {
+            return NextResponse.json({error:"All field required"},{status:400})
+            
+        }
+        const existingUser=await User.findOne({email})
+        if (existingUser) {
+            return NextResponse.json({error:"User Already Exist"},{status:400})
+            
+        }
+        const hashPass=await hashedPassword(password)
+        const user=await User.create({name,email,password:hashPass,role})
+        return NextResponse.json({message:"User Created" ,user:{id:user._id,email:user.email }})
         
         
     } catch (error) {
