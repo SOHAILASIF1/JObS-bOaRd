80757ec4315be098272b096e05156dc2683c355a
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-09 07:59:34 +0500

    auto commit

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,2 +1,14 @@
 import jwt from "jsonwebtoken"
 import bcrypt from 'bcryptjs'
+
+
+export async function hashedPassword(password:string) {
+    return bcrypt.hash(password,10)
+    
+}
+
+export async function comparePassword(password:string,hashed:string) {
+    return bcrypt.compare(password,hashed)
+    
+}
+
