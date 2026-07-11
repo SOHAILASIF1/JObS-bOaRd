e1df0f5923f794840a3cc26e1f9722eaebb61c0f
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-11 07:57:14 +0500

    auto commit

---
+++ app/(protected)/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { getUserFromToken } from "@/lib/getUserFromToken";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const user = await getUserFromToken();
+
+  if (!user) redirect("/login");
+
+  return (
+    <div>
+      <h1>Welcome to your Dashboard</h1>
+      <p>Role: {user.role}</p>
+    </div>
+  );
+}
