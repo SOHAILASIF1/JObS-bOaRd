65ca26bf9ca0797da19dbe14932ec881c3273092
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-11 08:06:01 +0500

    auto commit

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -10,6 +10,7 @@ export default async function DashboardPage() {
     <div>
       <h1>Welcome to your Dashboard</h1>
       <p>Role: {user.role}</p>
+      <p> User: {user.name} </p>
     </div>
   );
 }
