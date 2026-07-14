c98a8a2d558cc4ad148844eb324b80e612125465
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-14 08:43:42 +0500

    auto commit

--- app/api/job/route.ts
+++ app/api/job/route.ts
@@ -1,5 +1,7 @@
+import { connectDB } from "@/lib/dbConnection";
 import { getUserFromToken } from "@/lib/getUserFromToken";
 import { jobSchema } from "@/lib/validation/job";
+// import { connect } from "http2";
 import { NextResponse,NextRequest } from "next/server";
 
 
@@ -19,10 +21,21 @@ export async function POST(req:NextRequest){
         const body=await req.json()
         const parsed=jobSchema.safeParse(body)
         if (!parsed.success) {
-            return NextResponse
+            return NextResponse.json({
+                error:"Validation Failed",
+                issues:parsed.error.flatten().fieldErrors
+            },{status:400})
             
         }
+        await connectDB()
+        const job=await Job.create({
+            ...parsed.data,
+            employerId:user.id
+        })
+        return  NextResponse.json({message:"Job Posted",job},{status:201})
     } catch (error) {
+         console.error("Job post error:", err);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
         
     }
 }
