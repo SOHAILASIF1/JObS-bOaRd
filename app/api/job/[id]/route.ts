6101c80817c514fa0166f0d2613ea589a7e96977
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-15 23:09:39 +0500

    auto commit

--- app/api/job/[id]/route.ts
+++ app/api/job/[id]/route.ts
@@ -60,10 +60,10 @@ export async function PUT(req: NextRequest, { params }: { params: Promise<{ id: 
 
         }
         const updatedJob = await jobModel.findByIdAndUpdate(id, { ...validation.data, status: "pending" }, { new: true })
-        return NextResponse.json({message:"job Updated"},{status:200})
+        return NextResponse.json({ message: "job Updated" }, { status: 200 })
     } catch (error) {
-         console.error(err);
-    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+        console.error(err);
+        return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
 
     }
 }
