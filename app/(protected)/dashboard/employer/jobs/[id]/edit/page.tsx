833b44e3a0fc605fc510eb12a6957e40edce86fe
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-16 08:54:42 +0500

    auto commit

--- app/(protected)/dashboard/employer/jobs/[id]/edit/page.tsx
+++ app/(protected)/dashboard/employer/jobs/[id]/edit/page.tsx
@@ -1,6 +1,19 @@
+import { connectDB } from '@/lib/dbConnection'
+import { getUserFromToken } from '@/lib/getUserFromToken'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const EditJobPage = async() => {
+const EditJobPage = async({params}:{params:Promise<{id:string}>}) => {
+
+
+
+  const {id}=await params
+  const user=await getUserFromToken()
+  if (!user||user.role!=="employer") {
+    redirect("/login")
+    
+  }
+  await connectDB()
   return (
     <div>page</div>
   )
