0e8f92d2c4d5d97b267a8ef9b48ca0ee2bfb4413
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-17 17:15:52 +0500

    auto commit

--- Component/DeleteJobButton.tsx
+++ Component/DeleteJobButton.tsx
@@ -2,31 +2,31 @@
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 
-function DeleteJobButton({jobId}:{jobId:string}) {
-    const [loading,setLoading]=useState(false)
-    const router=useRouter()
-    const handleDelete=async ()=>{
-        const confirmed=window.confirm("Did you really want to delete")
-        if (!confirmed) return
-        setLoading(true)
-        try {
-          const res=await fetch(`api/job/${jobId}`,{
-            method:"DELETE"
-          })
-          const data=await res.json()
-          if (!res.ok) {
-            alert(data.error || "Delete Failed")
-            setLoading(false)
-            return
-            
-          }
-          router.refresh()
-        } catch (error) {
-          alert("Kuch masla ho gaya, dobara try karein")
-            setLoading(false)
-          
-        }
+function DeleteJobButton({ jobId }: { jobId: string }) {
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
+  const handleDelete = async () => {
+    const confirmed = window.confirm("Did you really want to delete")
+    if (!confirmed) return
+    setLoading(true)
+    try {
+      const res = await fetch(`api/job/${jobId}`, {
+        method: "DELETE"
+      })
+      const data = await res.json()
+      if (!res.ok) {
+        alert(data.error || "Delete Failed")
+        setLoading(false)
+        return
+
+      }
+      router.refresh()
+    } catch (error) {
+      alert("Kuch masla ho gaya, dobara try karein")
+      setLoading(false)
+
     }
+  }
 
   return (
     
