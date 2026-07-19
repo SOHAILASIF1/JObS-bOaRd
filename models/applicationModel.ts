c2203c530d798c71395739721df8e954d3a336ad
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-19 08:58:31 +0500

    auto commit

---
+++ models/applicationModel.ts
@@ -0,0 +1,24 @@
+import { model, models, Schema } from "mongoose";
+
+const applicationSchema=new Schema({
+    jobId:{
+        type:Schema.Types.ObjectId,
+        ref:"Job",
+        required:true
+    },
+    candidateId:{
+        type:Schema.Types.ObjectId,
+        ref:User,
+        required:true
+    },
+    resumeLink:{type:String,required:true},
+    status:{
+        type:String,
+        enum:["pending","rejected","shortlisted","hired"]
+        default:"pending"
+    }
+},{timestamps:true});
+
+
+applicationSchema.index({jobId:1,candidateId:1},{unique:true})
+export default models.Application||model("Appliction",applicationSchema)
