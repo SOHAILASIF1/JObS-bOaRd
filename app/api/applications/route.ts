621740e53ac1b3036de2173adc40f51dbd8a6574
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-20 17:58:10 +0500

    auto commit

--- app/api/applications/route.ts
+++ app/api/applications/route.ts
@@ -15,8 +15,32 @@ export async function POST(req:NextRequest) {
         }
         const body=await req.json()
         const parsed=applicationSchema.safeParse(body)
-        if
+        if (!parsed.success) {
+            return NextResponse.json({
+                error:"Validation Failed" ,issues:parsed.error.flatten().fieldErrors
+            },{status:400})
+            
+        }
+        const {jobId,resumeLink}=parsed.data
         await connectDB()
+        const job=await jobModel.findById(jobId)
+        if (!job) {
+            return NextResponse.json({error:"job not found"},{status:404})
+            
+        }
+        const already = await applicationModel.findOne({ jobId, candidateId: user.id });
+    if (already) {
+      return NextResponse.json(
+        { error: "Aap is job pe pehle hi apply kar chuke hain" },
+        { status: 400 }
+      );
+    }
+
+    const application = await applicationModel.create({
+      jobId,
+      candidateId: user.id,
+      resumeLink,
+    });
         
     } catch (error) {
         
