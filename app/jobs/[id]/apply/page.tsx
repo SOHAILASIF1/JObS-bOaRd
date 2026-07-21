90c6cbe315e11f25b173396b73386c8e01dbb5bb
SOHAILASIF1 <alyana998877@gmail.com>
2026-07-21 07:32:40 +0500

    auto commit

--- app/jobs/[id]/apply/page.tsx
+++ app/jobs/[id]/apply/page.tsx
@@ -1,7 +1,17 @@
+import { connectDB } from '@/lib/dbConnection'
+import { getUserFromToken } from '@/lib/getUserFromToken'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 function ApplyPage({params}:{params:Promise<{id:string}>}) {
        const {id}=await params
+       const user=await getUserFromToken()
+       if (!user||user.role!=="candidate") {
+        redirect("/login")
+        
+       }
+       connectDB()
+
   return (
  
   
